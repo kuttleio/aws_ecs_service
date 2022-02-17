cf730aa2c784dda9a1c7d5f20933cdec2a72041b
Dmitriy <dspv@me.com>
2022-02-17 17:08:50 +0200

    Remove public_ip

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_ecs_service" "main" {
   network_configuration {
     security_groups   = var.security_groups
     subnets           = var.subnets
-    assign_public_ip  = var.public
+    # assign_public_ip  = var.public
   }
 
   load_balancer {
